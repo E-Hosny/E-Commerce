0cd9ffd351acb818e30a0c831ee68a430b9a4426
E-hosny <ebrahimhosny511@gmail.com>
2024-01-07 21:24:36 +0300

    third commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,11 +21,23 @@ export default function Navbar()
      }
    
     return<> 
-      <nav className="navbar navbar-expand-sm bg-body-tertiary">
+      <nav className="navbar navbar-expand bg-body-tertiary">
  <div className="container-fluid">
     <div className="collapse navbar-collapse d-flex align-items-center justify-content-center" id="navbarSupportedContent" >
       <ul className="navbar-nav me-auto mb-2 fw-bold test">
-        <img src={logo} alt="logo" className="px-3" />
+        <div className="myLogo">
+           <img src={logo} alt="logo" className="px-3" />
+        </div>
+        <div className="myIcon">
+          <i className="fab fa-facebook m-2 " ></i>
+          <i className="fab fa-twitter m-2 " ></i>
+          <i className="fab fa-instagram m-2 " ></i>
+          <i className="fab fa-tiktok m-2 " ></i>
+          <i className="fab fa-linkedin m-2 " ></i>
+          <i className="fab fa-youtube m-2 " ></i>
+        </div>
+        
+        
 
        {userToken!==null ?
        <>
@@ -41,14 +53,7 @@ export default function Navbar()
          </li> </>:'' }
       </ul>
        
-      <div className=" ms-auto px-3">
-          <i className="fab fa-facebook m-2 " ></i>
-          <i className="fab fa-twitter m-2 " ></i>
-          <i className="fab fa-instagram m-2 " ></i>
-          <i className="fab fa-tiktok m-2 " ></i>
-          <i className="fab fa-linkedin m-2 " ></i>
-          <i className="fab fa-youtube m-2 " ></i>
-        </div>
+      
      <div>
        <ul className="navbar-nav ms-auto mb-2 fw-bold">
 
@@ -66,9 +71,11 @@ export default function Navbar()
       </li>
        </>}
       </ul>
+     
     </div>
         
     </div>
+   
   </div>
 </nav>
   </>
